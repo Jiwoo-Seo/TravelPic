5b377fd4dece02a531c405618e42ad2b592e3ee6
hyunjo325 <hyunjo325@naver.com>
2024-06-14 21:31:39 +0900

    앨범 참여 기능 조금

--- app/src/main/java/com/example/travelpic/Screen/Screen1.kt
+++ app/src/main/java/com/example/travelpic/Screen/Screen1.kt
@@ -1,5 +1,6 @@
 package com.example.travelpic.Screen
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
@@ -23,6 +24,11 @@ import com.example.travelpic.navViewmodel
 import com.example.travelpic.roomDB.AlbumCode
 import com.example.travelpic.userAlbumViewModel.MyAlbumList
 import com.example.travelpic.userAlbumViewModel.UserAlbumViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.Random
 
 @Composable
@@ -31,7 +37,9 @@ fun Screen1(navController: NavController, albumViewModel: AlbumViewModel, userAl
     val backgroundImage: Painter = painterResource(id = R.drawable.background_image) // 배경 이미지 리소스
     var showNewButtons by remember { mutableStateOf(false) }
     var showDialog by remember { mutableStateOf(false) }
+    var showJoinDialog by remember { mutableStateOf(false) }
     var albumName by remember { mutableStateOf("") }
+    var albumCode by remember { mutableStateOf("") }
     val navViewModel: navViewmodel = viewModel(viewModelStoreOwner = LocalNavGraphViewModelStoreOwner.current)
 
     var expanded by remember { mutableStateOf(false) }
@@ -96,7 +104,7 @@ fun Screen1(navController: NavController, albumViewModel: AlbumViewModel, userAl
                             Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "앨범 참여하기")}
                     }, onClick = {
                         expanded = false
-                        
+                        showJoinDialog = true
                         })
                 }
             }
@@ -128,6 +136,7 @@ fun Screen1(navController: NavController, albumViewModel: AlbumViewModel, userAl
                     userAlbumViewModel.addAlbumCode(newAlbumCode)
                     navViewModel.albumname = albumName
                     navViewModel.albumcode = newAlbum.code
+                    albumName = ""
                     navController.navigate("screen2")
                     showDialog = false
                 }) {
@@ -136,6 +145,55 @@ fun Screen1(navController: NavController, albumViewModel: AlbumViewModel, userAl
             },
             dismissButton = {
                 Button(onClick = { showDialog = false }) {
+                    Text("취소")
+                }
+            }
+        )
+    }
+    if (showJoinDialog) {
+        AlertDialog(
+            onDismissRequest = { showJoinDialog = false },
+            title = { Text("앨범 참여하기") },
+            text = {
+                Column {
+                    Text("앨범 코드을 입력하세요:")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    OutlinedTextField(
+                        value = albumCode,
+                        onValueChange = { albumCode = it },
+                        label = { Text("앨범 코드") }
+                    )
+                }
+            },
+            confirmButton = {
+                var albumname by remember { mutableStateOf("") }
+                Button(onClick = {
+                    Log.i("albumcode",albumCode)
+                    val ref = Firebase.database.getReference("AlbumList/${albumCode}/name")
+                    ref.child("memo").get().addOnSuccessListener { dataSnapshot ->
+                        if (dataSnapshot.exists()) {
+                            albumname = dataSnapshot.getValue(String::class.java).toString()
+                            // memoValue를 사용하여 원하는 작업을 수행할 수 있습니다.
+                            // 예: MutableState로 저장하여 Compose UI에 반영하는 등의 작업
+                        } else {
+                            // 데이터가 없는 경우 처리
+                        }
+                    }.addOnFailureListener { exception ->
+                        // 데이터를 가져오는 도중 에러가 발생한 경우 처리
+                    }
+                    val newAlbumCode = AlbumCode(albumCode, albumname)
+                    userAlbumViewModel.addAlbumCode(newAlbumCode)
+                    navViewModel.albumname = albumName
+                    navViewModel.albumcode = albumCode
+                    albumCode = ""
+                    navController.navigate("screen2")
+                    showJoinDialog = false
+                }) {
+                    Text("확인")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { showJoinDialog = false }) {
                     Text("취소")
                 }
             }
